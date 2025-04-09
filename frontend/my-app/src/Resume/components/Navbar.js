eaddb77078af93a7cedcce7c9788113fb2b23dfd
SaiVinay023 <saivinay023@gmail.com>
2025-04-10 01:57:53 +0200

    simplified navbar code

--- frontend/my-app/src/Resume/components/Navbar.js
+++ frontend/my-app/src/Resume/components/Navbar.js
@@ -6,43 +6,25 @@ const Navbar = () => {
   return (
     <AppBar position="fixed" sx={{ top: 0, left: 0, width: '100%', backgroundColor: '#1976d2', boxShadow: 3 }}>
       <Toolbar>
-        <Container>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          </Typography>
-          <Button color="inherit" component={Link} to="/" sx={{ margin: 1 }}>
-            Home
-          </Button>
-          <Button color="inherit" component={Link} to="/about" sx={{ margin: 1 }}>
-            About Me
-          </Button>
-          <Button color="inherit" component={Link} to="/education" sx={{ margin: 1 }}>
-            Education
-          </Button>
-          <Button color="inherit" component={Link} to="/skills" sx={{ margin: 1 }}>
-            Skills
-          </Button>
-          <Button color="inherit" component={Link} to="/projects" sx={{ margin: 1 }}>
-            Projects
-          </Button>
-        {/*  <Button color="inherit" component={Link} to="/resume" sx={{ margin: 1 }}>
-            Resume/CV
-          </Button> */}
-         
-          
-          <Button color="inherit" component={Link} to="/hobbies" sx={{ margin: 1 }}>
-            Hobbies
-          </Button>
-          
-          {/*  <Button color="inherit" component={Link} to="/achievements" sx={{ margin: 1 }}>
-            Achievements
-          </Button> */}
-          <Button color="inherit" component={Link} to="/experiences" sx={{ margin: 1 }}>
-            Experiences
-          </Button>
-          <Button color="inherit" component={Link} to="/contact" sx={{ margin: 1 }}>
-            Contact Me
-          </Button>
-        </Container>
+      <Container sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    <Typography variant="h6" sx={{ flexGrow: 1 }}>
+      {/* Logo or Title here */}
+    </Typography>
+
+    {/* Navigation Buttons Group */}
+    <Box sx={{ display: 'flex', gap: 2 }}>
+      <Button color="inherit" component={Link} to="/">Home</Button>
+      <Button color="inherit" component={Link} to="/about">About Me</Button>
+      <Button color="inherit" component={Link} to="/education">Education</Button>
+      <Button color="inherit" component={Link} to="/skills">Skills</Button>
+      <Button color="inherit" component={Link} to="/projects">Projects</Button>
+      {/* <Button color="inherit" component={Link} to="/resume">Resume/CV</Button> */}
+      <Button color="inherit" component={Link} to="/hobbies">Hobbies</Button>
+      {/* <Button color="inherit" component={Link} to="/achievements">Achievements</Button> */}
+      <Button color="inherit" component={Link} to="/experiences">Experiences</Button>
+      <Button color="inherit" component={Link} to="/contact">Contact Me</Button>
+    </Box>
+  </Container>
       </Toolbar>
     </AppBar>
   );
